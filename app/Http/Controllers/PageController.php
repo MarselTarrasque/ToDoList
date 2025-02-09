49308f3a29fdfbc7656795344ebad709c8ff1eba
Марсель <kabaler2006@gmail.com>
2025-02-09 16:33:06 +0300

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //PAGES:
+    //HomePage
+    //Create a task
+    //Display a list of tasks
+    
+}
